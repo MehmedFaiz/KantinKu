129be14f4f47dd091b1a3c5326177d18662f4815
MohammedFaiz23 <madfaiz323@student.ub.ac.id>
2026-05-21 21:25:17 +0700

    Change redirect URL in login script
    Updated the redirect URL after successful login.

--- LoginDesktop/script.js
+++ LoginDesktop/script.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
             btnLogin.disabled = true;
 
             setTimeout(() => {
-                window.location.href = "../DashboardAdminDesktop/";
+                window.location.href = "../DashboardAdminDesktop/index.html";
             }, 1500);
         }
     });
