d18a791b634dd5f406ca1611769d977bab18845c
MohammedFaiz23 <madfaiz323@student.ub.ac.id>
2026-05-21 21:41:02 +0700

    Fix redirect URL in login script

--- LoginDesktop/script.js
+++ LoginDesktop/script.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
             btnLogin.disabled = true;
 
             setTimeout(() => {
-                window.location.href = "../LoginDesktop/DashboardAdminDesktop/index.html";
+                window.location.href = "../DashboardAdminDesktop/index.html";
             }, 1500);
         }
     });
